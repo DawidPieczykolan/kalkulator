69bd4d26a84b81da085d101b89cdd233fc08e07d
Dawid Pieczykolan <dawid125w36@outlook.com>
2022-07-15 12:19:20 +0200

    wersja 0.1.2a Dodano: -klawiaturę -interfejs

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    TextView resultTV,solutionTv;
+    MaterialButton buttonC,buttonBrackOpen,buttonBrackClose;
+    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonEquals;
+    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
+    MaterialButton buttonAC,buttonDot;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTV =findViewById(R.id.result_tv);
+        solutionTv=findViewById(R.id.soulution_tv);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
